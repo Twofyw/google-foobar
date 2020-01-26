a4521fbf138ae9fff769c645efff07e51efa5d1e
Twofyw Yang <ywywywx@gmail.com>
2020-01-26 18:18:21 +0800

    l3c2

--- level3/challenge2/solution.py
+++ level3/challenge2/solution.py
@@ -1,33 +1,49 @@
 import time
 
-
 def solution(l):
-    """
-    Is l sorted?
+    result = 0
+    dp = [0] * len(l)
+    for i in xrange(len(l) - 1):
+        for j in xrange(i + 1, len(l)):
+            if l[j] % l[i] == 0:
+                dp[j] += 1
+                result += dp[i]
+    return result
 
-    :param l:
-    :return:
-    """
-    lucky_tuples = {}
-    for i, o in enumerate(l):
-        seconds = []
-        for j, p in enumerate(l[i+1:]):  # Can be optimized to reuse it's factors
-            if p % o == 0:
-                seconds.append(((i + 1) + j, p))
-        # print('seconds', seconds)
-        lucky_tuples[(i, o)] = seconds
-    # print('lucky_tuples', lucky_tuples)
-
-    # build triplets
-    time.sleep(1)
-    lucky_triplets = []
-    for i, first in enumerate(l):
-        # print('first', first, lucky_tuples[first])
-        for j, second in lucky_tuples[(i, first)]:
-            # print('second', second)
-            for k, third in lucky_tuples[(j, second)]:
-                lucky_triplets.append((first, second, third))
-    return len(lucky_triplets)
+# def solution(l):
+#     """
+#     Is l sorted?
+#
+#     :param l:
+#     :return:
+#     """
+#     lucky_tuples = {}
+#     lucky_tuples_inv = {}
+#     for i, o in enumerate(l):
+#         seconds = []
+#         if o in lucky_tuples_inv:
+#             for j, p in lucky_tuples[lucky_tuples_inv[o]]:
+#                 if p % o == 0:
+#                     seconds.append((j, p))
+#         else:
+#             firsts = []
+#             for j, p in enumerate(l[i+1:]):  # Can be optimized to reuse it's factors
+#                 if p % o == 0:
+#                     seconds.append(((i + 1) + j, p))
+#         # print('seconds', seconds)
+#         lucky_tuples[(i, o)] = seconds
+#     # print('lucky_tuples', lucky_tuples)
+#
+#     # build triplets
+#     time.sleep(1)
+#     lucky_triplets = []
+#     for i, first in enumerate(l):
+#         # print('first', first, lucky_tuples[first])
+#         for j, second in lucky_tuples[(i, first)]:
+#             # print('second', second)
+#             for k, third in lucky_tuples[(j, second)]:
+#                 lucky_triplets.append((first, second, third))
+#     return len(lucky_triplets)
 
 
 if __name__ == '__main__':
